469a5b2a25abcbd1b49dfb671e4ffe9e4ffcfe49
Jeyabal <jeyabal1993@gmail.com>
2026-02-26 23:11:01 -0500

    Adding CORS settings for local dev

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/spellsRouter.js'
 
 const app = express();
-
+app.use(
+    cors({
+        origin: 'http://localhost:5173', //frontend codebase URL
+        methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    })
+);
 app.use(express.json());
 app.use('/', router);
 
